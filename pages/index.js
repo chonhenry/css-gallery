a59f3cba3e24736fced1993f72341285ee04b253
Henry Chon 2 <chonhenry@outlook.com>
2021-07-07 22:32:35 -0400

    pagination

--- pages/index.js
+++ pages/index.js
@@ -5,31 +5,81 @@ import styles from "../styles/Home.module.css";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Display from "../components/Display";
+import Button from "@material-ui/core/Button";
 import { createClient } from "contentful";
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
+//   const client = createClient({
+//     space: process.env.CONTENTFUL_SPACE_ID,
+//     accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+//   });
+
+//   const res = await client.getEntries({
+//     content_type: "cssDesign",
+//     order: "-sys.createdAt",
+//     limit: 3,
+//     skip: 3,
+//   });
+//   return {
+//     props: {
+//       css_design: res.items,
+//     },
+//     revalidate: 1,
+//   };
+// }
+
+const design_per_page = 2;
+
+export async function getServerSideProps({ query: { page = 1 } }) {
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_KEY,
   });
 
+  // const design_per_page = 2;
+  const skip = +page === 1 ? 0 : (+page - 1) * design_per_page;
+
   const res = await client.getEntries({
     content_type: "cssDesign",
     order: "-sys.createdAt",
+    limit: design_per_page,
+    skip: skip,
   });
+
   return {
     props: {
       css_design: res.items,
+      total_entries: res.total,
+      page,
     },
-    revalidate: 1,
   };
 }
 
-export default function Home({ css_design }) {
+export default function Home({ css_design, total_entries, page }) {
   const router = useRouter();
+  console.log(total_entries);
 
   return (
     <Container>
+      <br></br>
+      {total_entries - page * design_per_page > 0 && (
+        <Link href={`/?page=${+page + 1}`}>
+          <Button variant="outlined" color="primary">
+            Next Page
+          </Button>
+        </Link>
+      )}
+      {page > 1 && (
+        <Link href={`/?page=${+page - 1}`}>
+          <Button variant="outlined" color="primary">
+            Previous Page
+          </Button>
+        </Link>
+      )}
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
       <Grid container spacing={3}>
         {css_design.map((design) => {
           const html = design.fields.html.content[0].content[0].value;
