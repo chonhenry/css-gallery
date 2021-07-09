9aebed569c66dcc8747c61895b07d9cafcd57894
Henry Chon 2 <chonhenry@outlook.com>
2021-07-09 16:39:17 -0400

    next and previous page button

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,14 @@ import Display from "../components/Display";
 import Button from "@material-ui/core/Button";
 import CodeIcon from "@material-ui/icons/Code";
 import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Input from "@material-ui/core/Input";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import KeyboardArrowLeftIcon from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
 import { createClient } from "contentful";
 
-const design_per_page = 4;
+const design_per_page = 6;
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -22,7 +27,11 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    // margin: "1px",
+    marginTop: "20px",
+  },
+  textfield: {
+    width: "80px",
+    // height: "1px",
   },
 }));
 
@@ -51,28 +60,19 @@ export async function getServerSideProps({ query: { page = 1 } }) {
 }
 
 export default function Home({ css_design, total_entries, page }) {
+  const [currentPage, setCurrentPage] = useState(page);
   const router = useRouter();
   const total_pages = Math.floor(total_entries / design_per_page) + 1;
   const classes = useStyles();
 
+  const handleChange = (p) => {
+    if ((p > 0 && p <= total_pages) || p === "") setCurrentPage(p);
+  };
+
   return (
     <Container className={classes.container}>
-      <br></br>
-      {total_entries - page * design_per_page > 0 && (
-        <Link href={`/?page=${+page + 1}`}>
-          <Button variant="outlined">Next Page</Button>
-        </Link>
-      )}
-      {page > 1 && (
-        <Link href={`/?page=${+page - 1}`}>
-          <Button variant="outlined">Previous Page</Button>
-        </Link>
-      )}
-      {/* <br></br>
-      <br></br> */}
       {/* <p>total pages: {total_pages}</p> */}
-      {/* <br></br>
-      <br></br> */}
+
       <Grid container>
         {css_design.map((design) => {
           const html = design.fields.html.content[0].content[0].value;
@@ -88,7 +88,7 @@ export default function Home({ css_design, total_entries, page }) {
               xs={12}
               sm={6}
               md={4}
-              lg={3}
+              // lg={3}
               key={design.fields.slug}
             >
               <div className={styles.header}>
@@ -101,13 +101,40 @@ export default function Home({ css_design, total_entries, page }) {
                 html={html}
                 css={css}
                 javascript={javascript}
-                width={300}
-                height={300}
+                width={350}
+                height={350}
               />
             </Grid>
           );
         })}
       </Grid>
+
+      <form noValidate autoComplete="off" className={styles.btn_container}>
+        <Link href={`/?page=${+page - 1}`}>
+          <Button disabled={page <= 1}>
+            <KeyboardArrowLeftIcon />
+          </Button>
+        </Link>
+
+        <Input
+          className={classes.textfield}
+          id="outlined-basic"
+          variant="filled"
+          type="number"
+          value={currentPage}
+          InputProps={{ inputProps: { min: 0, max: 10 } }}
+          onChange={(e) => handleChange(e.target.value)}
+          endAdornment={
+            <InputAdornment position="end">{` / ${total_pages}`}</InputAdornment>
+          }
+        />
+
+        <Link href={`/?page=${+page + 1}`}>
+          <Button disabled={total_entries - page * design_per_page <= 0}>
+            <KeyboardArrowRightIcon />
+          </Button>
+        </Link>
+      </form>
     </Container>
   );
 }
